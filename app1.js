dbbfa68cd60e587e05bdabeccfd10ec9b7807ec8
AshleyThomasDevelopment <ashleythomasdevelopment@gmail.com>
2022-02-19 02:15:57 -0500

    Builds basic test suite
    Only workable for in-file scripts right now. Wrote out tests for
    functions not yet created. Followed the errors path generating the code
    for the solution. Ingenious.

---
+++ app1.js
@@ -0,0 +1,5 @@
+// App1 file for Test Driven Development practice
+
+function simpleLog(input) {
+    return input
+}
---
+++ test1.js
@@ -0,0 +1,111 @@
+// Test1 file for Test Driven Development practice
+
+
+// write function that takes an app function as an input, 
+// checks against expected output, and returns a pass/fail
+// message, and the expected and actual return
+function takeMyFunction(appFunction, input, expectedReturn, expectedFail = false) {
+    input instanceof Array ? (funcReturn = appFunction(...input)) : (funcReturn = appFunction(input))
+    funcReturn instanceof Array && (funcReturn = `${funcReturn}`) // should split variable off so that actual return value is still available
+    expectedReturn instanceof Array && (expectedReturn = `${expectedReturn}`)
+    funcReturn === expectedReturn & !expectedFail ? (message = "PASS") : (message = "**FAIL**")
+    message === "**FAIL**" & expectedFail && (message = "PASS")
+    testCount++
+    testLogger(message, expectedReturn, funcReturn)
+}
+
+function testLogger(message, expectedReturn, funcReturn) {
+    console.log(`\n\nTest ${testCount}:`)
+    console.log(`Result: ${message}`)
+    if (message === '**FAIL**') {
+        console.log('Expect value: ' + expectedReturn)
+        console.log(`Expect Type: ${typeof(expectedReturn)}`)
+        console.log(`Actual value: ${funcReturn}`)
+        console.log(`Actual Type: ${typeof(funcReturn)}`)
+    }
+}
+
+let testCount = 0
+
+function runTests() {
+    console.log('\nTest Suite Initialized')
+    testCount = 0
+
+    // 1
+    takeMyFunction(inputToOutput, 'hi dave', 'hi dave')
+
+    // 2 - expect failure
+    takeMyFunction(inputToOutput, 'hi ash', 'hi dave', true)
+
+    // 3 - expect failure
+    takeMyFunction(returnTypeString, null, "string", true)
+
+    // 4
+    takeMyFunction(returnTypeString, null, "string")
+    
+    // 5
+    takeMyFunction(innerFrameGenerator, ['red', 4, 5, 'vertical'], ['Vertical Red Rectangle', 18])
+
+    // 6
+    takeMyFunction(innerFrameGenerator, ['greenish', 4, 5, 'diagonal'], ['Diagonal Greenish Rectangle', 18])
+
+    // 6 - expect failure
+    takeMyFunction(innerFrameGenerator, ['greenish', 2, 5, 'diagonal'], ['Diagonal Greenish Rectangle', 18], true)
+
+    // 6 
+    takeMyFunction(innerFrameGenerator, ['grenish', 20, 5, 'diagonal'], ['Diagonal Greenish Rectangle', 50])
+
+    // 7 - expect failure - unknown how object will log
+    takeMyFunction(objectFrameGenerator, ['blue', 10, 12, 'rectangular'], ['Rectangular Blue shape that is 44 units around.', undefined], true)
+
+    // 7 - duplicate as above without expected fail
+    takeMyFunction(objectFrameGenerator, ['blue', 10, 12, 'rectangular'], ['Rectangular Blue shape that is 44 units around.', "[object Object]"])
+
+}
+
+runTests()
+
+
+
+
+
+// Mock Application functions
+
+function inputToOutput(input) {
+    return input
+}
+
+function returnTypeString() {
+    let obj = 'This string'
+    return typeof(obj)
+}
+
+function innerFrameGenerator(color, length, width, orientation) {
+    let colorValue = color[0].toUpperCase() + color.substring(1)
+    let position = orientation[0].toUpperCase() + orientation.substring(1)
+    let description = `${position} ${colorValue} Rectangle`
+    let perimeter = length * 2 + width * 2
+    let output = [description, perimeter]
+    return output
+}
+
+function objectFrameGenerator(color, length, width, type) {
+    let shape = rectGen(length, width, type)
+    let colorValue = color[0].toUpperCase() + color.substring(1)
+    let shapeType = shape.type[0].toUpperCase() + shape.type.substring(1)
+    let perimeter = length * 2 + width * 2
+    let description = `${shapeType} ${colorValue} shape that is ${perimeter} units around.`
+    return [description, shape]
+}
+
+function rectGen(length, width, input) {
+    class Rectangle {
+        constructor(length, width, input) {
+            this.length = length
+            this.width = width
+            this.type = input
+        }
+    }    
+    let rect = new Rectangle(length, width, input)
+    return rect
+}
