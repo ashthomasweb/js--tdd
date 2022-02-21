cf4e4e19a6fb41d161988726213a75ea2119ba06
AshleyThomasDevelopment <ashleythomasdevelopment@gmail.com>
2022-02-21 00:55:30 -0500

    Corrects Expect fail function
    Was not properly failing on pass when expected.

--- test1.js
+++ test1.js
@@ -1,6 +1,43 @@
 // Test1 file for Test Driven Development practice
 
 
+function logPass() {
+    return logColor('green', 'Pass')
+}
+
+function logFail() {
+    return logColor('red', '**Fail**')
+}
+
+function logColor(color, input) {
+    let beginColor = '\033'
+    let colorCode = ''
+    let reset = '\033[39m'
+    switch (color) {
+        case 'red':
+          colorCode = '[31m'
+          break;
+        case 'green':
+            colorCode = `[32m`
+          break;
+        case 'yellow':
+            colorCode = `[33m`
+          break;
+        case 'blue':
+            colorCode = `[34m`
+          break;
+        case 'magenta':
+            colorCode = `[35m`
+          break;
+        case 'cyan':
+            colorCode = `[36m`
+            break;
+        default:
+            colorCode = `[39m`
+      }
+    return `${beginColor}${colorCode}${input}${reset}`
+}
+
 // write function that takes an app function as an input, 
 // checks against expected output, and returns a pass/fail
 // message, and the expected and actual return
@@ -8,15 +45,31 @@ function takeMyFunc(appFunction, input, expectedReturn, expectedFail = false) {
     input instanceof Array ? (funcReturn = appFunction(...input)) : (funcReturn = appFunction(input))
     let rawReturn = funcReturn
     funcReturn instanceof Array && (funcReturn = `${funcReturn}`) // should split variable off so that actual return value is still available
+    let rawExpect = expectedReturn
     expectedReturn instanceof Array && (expectedReturn = `${expectedReturn}`)
-    funcReturn === expectedReturn & expectedFail === false ? (message = "PASS") : (message = "**FAIL**")
-    message === "**FAIL**" & expectedFail && (message = "PASS")
+    funcReturn === expectedReturn ? (message = true) : (message = false)
+    if (expectedFail) {
+        if (message === false) {
+            (message = true)
+        } else {
+            (message = false)
+        }
+    } 
     testCount++
-    testLogger(message, expectedReturn, rawReturn, expectedFail)
+    testLogger(message, rawExpect, rawReturn, expectedFail)
 }
 
 function testLogger(message, expectedReturn, actualReturn, expectedFail) {
-
+    
+    setRed = () => {
+        message = logFail()
+        resultColor = 'red'
+    }
+    setGreen = () => {
+        message = logPass()
+        resultColor = 'green'
+    }
+    message ? setGreen() : setRed()
     function Result(value, type) {
         this.value = value;
         this.type = type;
@@ -25,22 +78,19 @@ function testLogger(message, expectedReturn, actualReturn, expectedFail) {
     let act = new Result(actualReturn, typeof(actualReturn))
     let resultTable = {}
     resultTable.expected = exp
-    resultTable.actual = act 
-
-    expectedFail ? (failValue = ' Expected to Fail:') : (failValue = '')
-    
-    console.log(`\n\nTest ${testCount}:`)
+    resultTable.actual = act
+    expectedFail ? (failValue = logColor('yellow', ' Anticipating error:')) : (failValue = '')
+    console.log(logColor(resultColor, `\n\nTest ${testCount}:`))
     console.log(`Results:${failValue} ${message}`)
-    if (message === '**FAIL**' || expectedFail ) {
+    if (message === logFail() || expectedFail ) {
         console.table(resultTable)
     }
 }
 
-
 let testCount = 0
 
 function runTests() {
-    console.log('\nTest Suite Initialized')
+    console.log('\n\033[33mTest Suite Initialized\033[39m')
     testCount = 0
 
     // 1
@@ -62,7 +112,7 @@ function runTests() {
     takeMyFunc(innerFrameGenerator, ['greenish', 4, 5, 'diagonal'], ['Diagonal Greenish Rectangle', 18])
 
     // 7 - expect failure
-    takeMyFunc(innerFrameGenerator, ['greenish', 2, 5, 'diagonal'], ['Diagonal Greenish Rectangle', 18], true)
+    takeMyFunc(innerFrameGenerator, ['greenish', 2, 5, 'diagonal'], ['Diagonal Greenish Rectangle', 14])
 
     // 8 
     takeMyFunc(innerFrameGenerator, ['greenish', 20, 5, 'diagonal'], ['Diagonal Greenish Rectangle', 50])
@@ -81,7 +131,6 @@ runTests()
 
 
 
-// console.log('\033[33m Hello World \033[39m');
 
 
 
